11de60abaea32d2371a4a0968e93127d9e2d5d67
Jose L. Moreno <jose0909@live.com>
2020-01-22 13:32:27 -0500

    pendiente cambio de rutas children

--- resources/js/router/routes/administrar.js
+++ resources/js/router/routes/administrar.js
@@ -4,17 +4,22 @@ export default [
         path: '/administrar',
         name: 'administrar.listar',
         component: page('administrar/administrar'),
-        children: [
-            {
-                path: 'areas/listar',
-                name: 'areas.listar',
-                component: page('administrar/areas/listar')
-            },
-            {
-                path: 'encargados/listar',
-                name: 'encargados.listar',
-                component: page('administrar/encargados/listar')
-            },
-        ]
+        // children: [
+        //     {
+        //         path: '/areas/listar',
+        //         name: 'areas.listar',
+        //         component: page('administrar/areas/listar')
+        //     },
+        //     {
+        //         path: '/encargados/listar',
+        //         name: 'encargados.listar',
+        //         component: page('administrar/encargados/listar')
+        //     },
+        // ]
+    },
+    {
+        path: '/areas/listar',
+        name: 'areas.listar',
+        component: page('administrar/areas/listar')
     }
 ]
