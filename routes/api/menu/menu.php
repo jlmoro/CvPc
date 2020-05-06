bc1a5e276270e05e2c93d3bde5bde04d76df18d8
jose l moreno <jose0909@live.com>
2020-05-06 11:43:43 -0500

    cambio pendiente

--- routes/api/menu/menu.php
+++ routes/api/menu/menu.php
@@ -2,6 +2,6 @@
 Route::group(['middleware' =>'auth:api'], function (){
     Route::prefix('menu-principal')->group( function(){
         $controlador = "MenuController";
-        Route::get("listar-menu","$controlador@listar_menu");       
+        Route::get("listar-menu","$controlador@listar_menu");
     });
 });
