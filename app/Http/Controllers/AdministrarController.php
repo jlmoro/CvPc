c3c0928979f16997f4cc74c4a59196b5e02f01dd
Jose L. Moreno <jose0909@live.com>
2020-06-04 14:23:30 -0500

    ajuste listado de administrar

--- app/Http/Controllers/AdministrarController.php
+++ app/Http/Controllers/AdministrarController.php
@@ -11,9 +11,10 @@ class AdministrarController extends Controller
     public function listar_administrar()
     {
         try {
-
-            return Administrar::all();
-            // return Administrar::select('nombre as label','icono as icon','ruta as to')->get();
+            // return Administrar::all();
+            return Administrar::select('*')
+            ->orderBy('posicion','ASC')
+            ->get();
 
         } catch (\Exception $e) {
             return $this->captura_error($e);
