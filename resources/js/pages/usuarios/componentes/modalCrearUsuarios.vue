7955940753e531f261c896620207bad0a7630c29
Jose L. Moreno <jose0909@live.com>
2020-07-06 12:17:43 -0500

    formulario crear usuarios

--- resources/js/pages/usuarios/componentes/modalCrearUsuarios.vue
+++ resources/js/pages/usuarios/componentes/modalCrearUsuarios.vue
@@ -10,33 +10,34 @@
       <div class="row w-100 mb-3 mt-2">
         <div class="col-12 text-center">
           <croppa
+          v-model="form.foto"
           placeholder="Seleccione una imagen"
           :placeholder-font-size="12"
           :width="140"
           :height="146"
           :show-remove-button="true"
           :prevent-white-space="true"
-          ref="CroppaProveedor"
           >
+          <!-- ref="CroppaProveedor" -->
         </croppa>
       </div>
     </div>
     <div class="row w-100 mb-1">
       <div class="col-6">
         <label for="nombre-prov">Nombre(s):</label>
-        <el-input type="text" placeholder="Nombre proveedor" v-model="form.name" maxlength="60" show-word-limit >
+        <el-input type="text" placeholder="Nombre(s)" v-model="form.name" maxlength="60" show-word-limit >
         </el-input>
       </div>
       <div class="col-6">
         <label for="nombre-prov">Apellido(s):</label>
-        <el-input type="text" placeholder="Nombre proveedor" v-model="form.lastname" maxlength="60" show-word-limit >
+        <el-input type="text" placeholder="Apellido(s)" v-model="form.lastname" maxlength="60" show-word-limit >
         </el-input>
       </div>
     </div>
     <div class="row w-100 mt-1">
       <div class="col-6">
         <label for="tipo_doc">Tipo Documento:</label>
-        <el-input type="text" placeholder="Tipo Doc" v-model="form.tipo_doc" maxlength="30" show-word-limit >
+        <el-input type="text" placeholder="Tipo Documento" v-model="form.tipo_doc" maxlength="30" show-word-limit >
         </el-input>
       </div>
       <div class="col-6">
@@ -60,7 +61,7 @@
     <div class="row w-100 mt-1">
       <div class="col-6">
         <label for="area">Area:</label>
-        <el-select v-model="value" filterable clearable placeholder="Select">
+        <el-select v-model="form.id_area" filterable clearable placeholder="Seleccione Área" @change="listar_roles($event)">
           <el-option
             v-for="(item,a) in areas"
             :key="a"
@@ -71,8 +72,14 @@
       </div>
       <div class="col-6">
         <label for="rol">Rol:</label>
-        <el-input type="text" placeholder="Rol" v-model="form.rol" maxlength="30" show-word-limit >
-        </el-input>
+        <el-select v-model="form.id_rol" filterable clearable placeholder="Seleccione Rol">
+          <el-option
+            v-for="(item,r) in roles"
+            :key="a"
+            :label="item.nombre"
+            :value="item.id">
+          </el-option>
+        </el-select>
       </div>
     </div>
     <div class="row w-100 mt-1">
@@ -105,39 +112,39 @@ export default {
   data(){
     return{
       form:{
-        logo:'',
+        foto:null,
+        roles:[],
+        id_area:null,
       },
-      options: [{
-        value: 'Option1',
-        label: 'Option1'
-      }, {
-        value: 'Option2',
-        label: 'Option2'
-      }, {
-        value: 'Option3',
-        label: 'Option3'
-      }, {
-        value: 'Option4',
-        label: 'Option4'
-      }, {
-        value: 'Option5',
-        label: 'Option5'
-      }],
-      value: ''
     }
   },
   methods:{
     async crear_proveedor(){
       try {
-        this.form.logo = this.$refs.CroppaProveedor.img.src
-        const {data} = await axios.post(`${this.ruta}/crear-proveedor`,this.form)
+        // this.form.logo = this.$refs.CroppaProveedor.img.src
+        const {data} = await axios.post(`${this.ruta}/crear-usuario`,this.form)
         this.form = {}
         this.$emit('proveedor:creado')
         this.$Helper.notificacion('success','Guardado',data.mensaje)
-        this.$refs.CroppaProveedor.img.src = ""
         this.$refs.modalProveedor.toggle()
       } catch (e) {
         console.warn(e);
+      }
+    },
+    async listar_roles(area){
+      this.form.id_rol = ''
+      console.log(area);
+      try {
+
+        const {data} = await axios(`/api/select/${area}/listar-roles`)
+        if (data.error) {
+          this.$Helper.notificacion('warning','Problemas al listar roles',data.error)
+          return
+        }
+        this.roles = data
+
+      } catch (e) {
+        return console.warn(e);
       }
     },
     toggle(){
--- routes/api/usuarios/usuarios.php
+++ routes/api/usuarios/usuarios.php
@@ -2,7 +2,7 @@
 Route::group(['middleware' =>'auth:api'], function (){
     Route::prefix('usuarios')->group( function(){
         $controlador = "UsuariosController";
-        Route::post("crear-proveedor","$controlador@crear_proveedor");
+        Route::post("crear-usuario","$controlador@crear_usuario");
         Route::get("listar-usuarios","$controlador@listar_usuarios");
         // Route::put("editar-invitado","$controlador@editar_invitado");
         Route::delete("{id_proveedor}/eliminar-proveedor","$controlador@eliminar_proveedor");
